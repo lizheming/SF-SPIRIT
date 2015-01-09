92f46c651f2acaa0da59fe6addf0bc60031faca6
lizheming <i@imnerd.org>
2015-01-09 19:03:25 +0800

    take sf's comment to emotion world

--- cmt-enhanced/main.js
+++ cmt-enhanced/main.js
@@ -5,7 +5,53 @@ switch(location.pathname.split("/")[1] || "") {
 		document.body.appendChild(s);
 	case "q":
 		var cmt = document.createElement("script");
-		cmt.innerHTML += 'window.onload=function(){(function cmtEnhanced(widgets){var widgets=[].slice.call(widgets),converter=require("pagedown_converter").Converter;var editor=new converter;widgets.forEach(function(widget){var preview=document.createElement("div"),textarea=widget.querySelector("textarea");preview.className="col-md-12 widget-comments__preview";widget.appendChild(preview);textarea.addEventListener("input",function(e){preview.innerHTML=editor.makeHtml(this.value)},false);textarea.addEventListener("keydown",function(e){if(e.ctrlKey&&e.keyCode===13)preview.innerHTML=""},false);widget.querySelector("button").addEventListener("click",function(){preview.innerHTML=""})})})(document.querySelectorAll(".widget-comments__form"))};';
+		cmt.innerHTML = 'var yan = '+JSON.stringify(yan)+';\r\n';
+		cmt.innerHTML += '\
+		window.onload = function() {\
+		(function cmtEnhanced(widgets) {\
+			var widgets = [].slice.call(widgets),\
+				converter = require("pagedown_converter").Converter,\
+				editor = new converter,\
+				emojList = document.createElement("div");\
+			\
+		    emojList.className = "widget-comments-emoj-list display";\
+		    yan.list.forEach(function(line) {\
+		        line.yan.forEach(function(em,i) { emojList.innerHTML += "<span>"+em+"</span>" });\
+		    });\
+		    widgets.forEach(function(widget) {\
+		        var preview = document.createElement("div"),\
+		        	emoj = document.createElement("div"),\
+		        	textarea = widget.querySelector("textarea"),\
+		        	widgetEmojList = emojList.cloneNode(true);\
+				\
+		        preview.className = "col-md-12 widget-comments__preview";\
+		        emoj.className = "col-md-2 widget-comments-emoj";\
+		        emoj.innerHTML = ">ω<";\
+				\
+		        widget.appendChild(emoj);\
+		        widget.appendChild(widgetEmojList);\
+		        widget.appendChild(preview);\
+		        widgetEmojList.addEventListener("click", function(e) {\
+		        	if(e.target.tagName.toUpperCase() != "SPAN") return true;\
+					textarea.value += e.target.innerHTML;\
+					this.classList.toggle("display");\
+					textarea.focus();\
+		        });\
+		        textarea.addEventListener("input", function(e) {\
+		            preview.innerHTML = editor.makeHtml(this.value)\
+		        });\
+		        textarea.addEventListener("keydown", function(e) {\
+		            if (e.ctrlKey && e.keyCode === 13) preview.innerHTML = ""\
+		        });\
+		        widget.querySelector("button").addEventListener("click", function() {\
+		            preview.innerHTML = ""\
+		        });\
+				emoj.addEventListener("click", function() {\
+					this.nextElementSibling.classList.toggle("display")\
+				})\
+		    })\
+		})(document.querySelectorAll(".widget-comments__form"))\
+		};';
 		document.body.appendChild(cmt);	
 	break;	
 }
