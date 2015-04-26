9f34c3d86afc02d010fb1896ea29947de063c12e
lizheming <secretlzm007@gmail.com>
2015-04-26 23:12:03 +0800

    clean

--- hotkey/main.js
+++
@@ -1,4 +0,0 @@
-$(document).on('keydown', '.pop-window', function(e) {
-	//New version has supported this hotkey!
-    //if(e.keyCode == 13) $('.pop-window .action').click() && e.preventDefault();
-});
--- reputation-chart-3d/main.js
+++
@@ -1,7 +0,0 @@
-chrome.storage.sync.get('reputationChart', function(d) {
-	if(+d.reputationChart) return false;
-	if(location.pathname.substr(0, 3) === "/u/") {
-		var chart = new sfISO();
-		chart.render();
-	}
-})
